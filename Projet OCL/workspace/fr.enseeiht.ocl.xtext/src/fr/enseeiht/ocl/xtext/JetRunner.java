249f5408c2e027cad06f4ef7b39687184b80794b
Guilhem Mangenot <guilhem.mangenot@gmail.com>
2025-02-24 15:22:49 +0100

    removed warnings

--- Projet OCL/workspace/fr.enseeiht.ocl.xtext/src/fr/enseeiht/ocl/xtext/JetRunner.java
+++ Projet OCL/workspace/fr.enseeiht.ocl.xtext/src/fr/enseeiht/ocl/xtext/JetRunner.java
@@ -11,7 +11,6 @@ import org.eclipse.emf.codegen.merge.java.JMerger;
 import org.eclipse.emf.codegen.merge.java.facade.JCompilationUnit;
 import org.eclipse.emf.codegen.merge.java.facade.jdom.JDOMFacadeHelper;
 import org.eclipse.emf.codegen.merge.java.facade.jdom.JDOMJCompilationUnit;
-import org.eclipse.emf.codegen.util.CodeGenUtil;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
