5af771757106ae58d1ffbff19f21154b6f233fce
PSOMSON <106836701+PSOMSON@users.noreply.github.com>
2025-03-03 17:54:20 +0100

    commentaire + doc

--- Projet OCL/workspace/fr.enseeiht.ocl.xtext/src/fr/enseeiht/ocl/xtext/types/OclTypePair.java
+++ Projet OCL/workspace/fr.enseeiht.ocl.xtext/src/fr/enseeiht/ocl/xtext/types/OclTypePair.java
@@ -4,12 +4,19 @@ import fr.enseeiht.ocl.xtext.OclType;
 
 /**
  * This class has no OCL equivalent, this is only a programming artifice to help with OCL elements that represent
- * elements with two types (like map elements : "(Type, Type)".
- * DO NOT treat this as a regula
+ * elements with two types (like map elements : "(Type, Type))".
+ * DO NOT treat this as a regular type. This is only used in very specific occasions (see MapElementValidationAdapter)
  */
 public class OclTypePair implements OclType {
 	
+	/**
+	 * The left element in the pair of type.
+	 */
 	private OclType left;
+	
+	/**
+	 * the right element in the pair of types.
+	 */
 	private OclType right;
 
 	public OclTypePair(OclType left, OclType right) {
